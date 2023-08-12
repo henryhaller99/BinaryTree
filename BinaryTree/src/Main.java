19ab37aa7446ffd94548f3cc2d67aa1596b03a07
henryhaller99 <henryhernandezbarajas@outlook.com>
2023-08-12 13:01:51 -0600

    BinaryTree class added and some implementations in public static void main(String[] args) added

--- BinaryTree/src/Main.java
+++ BinaryTree/src/Main.java
@@ -1,6 +1,14 @@
+import classes.BinaryTree;
+import classes.TreeNode;
 
 public class Main {
     public static void main(String[] args) {
+        TreeNode first = new TreeNode(1);
+        TreeNode second = new TreeNode(2);
+        TreeNode third = new TreeNode(3);
+        TreeNode fourth = new TreeNode(4);
+        TreeNode fifth = new TreeNode(5);
 
+        BinaryTree binaryTree = new BinaryTree(first);
     }
 }
---
+++ BinaryTree/src/classes/BinaryTree.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class BinaryTree {
+
+    private TreeNode root;
+
+    public BinaryTree(TreeNode root) {
+        this.root = root;
+    }
+}
