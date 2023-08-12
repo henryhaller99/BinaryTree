3f590f4d0cb1fa66ed922563805731410f62142f
henryhaller99 <henryhernandezbarajas@outlook.com>
2023-08-12 12:58:26 -0600

    TreeNode classes added

---
+++ BinaryTree/src/classes/TreeNode.java
@@ -0,0 +1,36 @@
+package classes;
+
+public class TreeNode {
+
+    private TreeNode leftNode;
+    private TreeNode rightNode;
+    private int value;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+
+    public TreeNode getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(TreeNode leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public TreeNode getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(TreeNode rightNode) {
+        this.rightNode = rightNode;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
